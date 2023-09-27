10b5be167dc600d027ad283a9ea45b15641a0e23
John Arroyo II <jtarroyo2@gmail.com>
2023-09-27 15:40:23 -0600

    progress on front end react

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import React from "react";
-import { App } from '/app.';
+import ReactDom from 'react-dom'
+import { App } from '/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
+const port = 3000
+
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to Artist`s Eye')
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.get('/myportfolio', (req, res) => {
+  res.status(200).send('My Portfolio')
+})
+
+app.post('/myportfolio', (req, res) => {
+  res.status(200)
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 })
